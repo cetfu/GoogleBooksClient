b9d394226df33fcf9b585687a33963a03a3034c7
Furkan ÇETİNKAYA <furkancetinkaya0746@gmail.com>
2025-05-16 17:51:33 +0300

    Set up base project structure and cleaned up files

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GoogleBooksClient extends StatelessWidget {
+  const GoogleBooksClient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Google Books Client",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
