e07a9f54fb4b54123511a72154233d43129cc88a
Furkan ÇETİNKAYA <furkancetinkaya0746@gmail.com>
2025-05-17 20:51:41 +0300

    BookDetailView design

--- lib/views/book_detail/widgets/book_detail_view.dart
+++ lib/views/book_detail/widgets/book_detail_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:google_books_client/models/book_model.dart';
 
 class BookDetailView extends StatelessWidget {
@@ -6,8 +7,78 @@ class BookDetailView extends StatelessWidget {
 
   final Book? book;
 
+  String _getBookThumbnail(ImageLinks? imageLinks) {
+    if (imageLinks != null && imageLinks.thumbnail != null) {
+      return imageLinks.thumbnail!;
+    }
+    return "";
+  }
+
+  String _getAuthors(List<String>? authors) {
+    if (authors == null) return "Bilinmeyen Yazar";
+    return authors.join(", ");
+  }
+
+  void _onFavourite() {}
+
   @override
   Widget build(BuildContext context) {
-    return Text("Book detail");
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 16,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .6,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                _getBookThumbnail(book?.volumeInfo.imageLinks),
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        book!.volumeInfo.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(_getAuthors(book!.volumeInfo.authors)),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(),
+              Row(
+                children: [
+                  Flexible(
+                    child: Html(
+                      data: "${book!.volumeInfo.description}",
+                      style: {
+                        "*": Style(
+                          margin: Margins.zero,
+                          padding: HtmlPaddings.zero,
+                        ),
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
