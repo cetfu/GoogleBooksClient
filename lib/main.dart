27142208b88e450d1e2cdd49a6ef961d3ea78c16
Furkan ÇETİNKAYA <furkancetinkaya0746@gmail.com>
2025-05-17 18:35:13 +0300

    Add BookDetailViewModel's ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_books_client/app.dart';
+import 'package:google_books_client/view_models/book_detail_view_model.dart';
 import 'package:google_books_client/view_models/book_list_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => BookListViewModel())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => BookListViewModel()),
+        ChangeNotifierProvider(create: (_) => BookDetailViewModel()),
+      ],
       child: const GoogleBooksClient(),
     ),
   );
