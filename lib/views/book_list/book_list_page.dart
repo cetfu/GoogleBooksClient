880892fa58a08e0a0053db9184ad3e12ea9efe34
Furkan ÇETİNKAYA <furkancetinkaya0746@gmail.com>
2025-05-17 18:00:42 +0300

    Integrate BookListViewModel and BookListPageGrid widget

--- lib/views/book_list/book_list_page.dart
+++ lib/views/book_list/book_list_page.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_books_client/core/common/layout_view.dart';
+import 'package:google_books_client/view_models/book_list_view_model.dart';
+import 'package:google_books_client/views/book_list/widgets/book_list_page_grid.dart';
+import 'package:provider/provider.dart';
 
 class BookListPage extends StatelessWidget {
   const BookListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LayoutView(child: Text("Ana Sayfa")));
+    final vm = Provider.of<BookListViewModel>(context, listen: false);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      vm.loadBooks("Tolkien", page: 0);
+    });
+
+    return Scaffold(
+      body: LayoutView(
+        child: Consumer<BookListViewModel>(
+          builder: (context, vm, child) {
+            if (vm.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return BookListPageGrid();
+          },
+        ),
+      ),
+    );
   }
 }
