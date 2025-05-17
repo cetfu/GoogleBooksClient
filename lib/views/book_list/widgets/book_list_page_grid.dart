3de3415fd0cbbed4972e67b387194e22135104cc
Furkan ÇETİNKAYA <furkancetinkaya0746@gmail.com>
2025-05-17 17:57:53 +0300

    Create book_list_page_grid.dart and book_list_page_grid_item.dart widgets

---
+++ lib/views/book_list/widgets/book_list_page_grid.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_books_client/view_models/book_list_view_model.dart';
+import 'package:google_books_client/views/book_list/widgets/book_list_page_grid_item.dart';
+import 'package:provider/provider.dart';
+
+class BookListPageGrid extends StatelessWidget {
+  const BookListPageGrid({super.key});
+
+  final SliverGridDelegate _gridDelegate =
+      const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 5,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      );
+
+  Widget? _itemBuilder(BuildContext context, int index) {
+    final vm = Provider.of<BookListViewModel>(context, listen: false);
+    final book = vm.books[index];
+    String bookID = book.id;
+    String bookTitle = book.volumeInfo.title;
+    String bookThumbnail = book.volumeInfo.imageLinks?.thumbnail ?? "";
+    List<String>? authors = book.volumeInfo.authors;
+
+    return BookListPageGridItem(
+      id: bookID,
+      title: bookTitle,
+      thumbnail: bookThumbnail,
+      authors: authors,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<BookListViewModel>(
+      builder: (context, vm, child) {
+        return GridView.builder(
+          itemCount: vm.books.length,
+          gridDelegate: _gridDelegate,
+          itemBuilder: _itemBuilder,
+        );
+      },
+    );
+  }
+}
---
+++ lib/views/book_list/widgets/book_list_page_grid_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class BookListPageGridItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String thumbnail;
+  final List<String>? authors;
+
+  const BookListPageGridItem({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.thumbnail,
+    required this.authors,
+  });
+
+  String _getAuthors(List<String>? authors) {
+    if (authors == null) return "";
+
+    if (authors.length > 1) {
+      return "${authors.first}...";
+    }
+    return authors.first;
+  }
+
+  final TextStyle _titleStyle = const TextStyle(fontWeight: FontWeight.w700);
+  final TextStyle _authorsStyle = const TextStyle(color: Colors.grey);
+
+  void _onTap(BuildContext context) {
+    print("tap $id");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _onTap(context),
+      child: Column(
+        spacing: 6,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                thumbnail,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: _titleStyle),
+              Text(_getAuthors(authors), style: _authorsStyle),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
