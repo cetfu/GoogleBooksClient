0b7d10e354d1bdd55f154ca64998394c79aff294
Furkan ÇETİNKAYA <furkancetinkaya0746@gmail.com>
2025-05-19 12:59:03 +0300

    Remove unused import, fix Github workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_books_client/app.dart';
 
-import 'package:google_books_client/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
